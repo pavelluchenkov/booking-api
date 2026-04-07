8fbad82deaffe6fa87668dde984e91a8c196e093
pavelluchenkov <pavel.luchenkov@gmail.com>
2026-04-07 19:04:18 +0300

    feat: add/ status endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,8 +11,13 @@ func pingHandler(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
 
 }
+func statusHandler(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application-json")
+	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+}
 func main(){
 	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/status", statusHandler)
 
 	log.Println("Server started on localhost 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil{
